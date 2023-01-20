12adb38389ae3bb9dbc76ed9edf9cce099e51306
AnnieXUZian <96567335+AnnieXUZian@users.noreply.github.com>
2023-01-20 14:37:36 -0600

    Update curl_clone.py

--- curl_clone.py
+++ curl_clone.py
@@ -7,97 +7,95 @@ counter = 10
 PORT = 80
 
 
-def check_https(address):    #helper function to check that it's not https
-    if("https://" in address):
+input_address = str(sys.argv[1])
+output_message = "Host:..."+"\n"
+sys.stdout.write(output_message)
+
+
+
+def get_html(input_address,requests):
+#Use sys to get the http link passed in
+    
+    if requests==10:
+        sys.exit(1)
+        
+    PORT = 80
+    
+    if("https://" in input_address): #check for https
       sys.stderr.write("Cannot intake an https")
       sys.exit(1)
 
-#Use sys to get the http link passed in
-input_address = str(sys.argv[1])
-
-output_message = "Host:..."+"\n"+input_address
-sys.stdout.write(output_message)
-
-check_https(input_address)
-
-if (input_address[-1]=="/"):
-    input_address = input_address[:-1]
-    
-if(':' in input_address[7:]): #with a port number
-    portP=(input_address[7:]).find(':')
-    PORT=input_address[portP+8:-1]
-    PORT=int(PORT)
-    input_address=input_address[:portP+7]
-
-HOST = input_address[7:]  #sys.argv[1]- gets us the address  str()- makes it a string [7:]- gets rid of http://
-
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-sock.connect_ex((HOST, PORT)) # connect to the host
-
-website_name = "GET / HTTP/1.1\r\nHost:"+HOST+"\r\n\r\n"
-
-sock.send(website_name.encode('utf-8'))
-
-response=sock.recv(4096)
-while True:
-    data=sock.recv(4096)
-    if not data:
-        break
-    else:
-        response = response.append(data)
+    if (input_address[-1]=="/"):#with / at the end of the URL
+        input_address = input_address[:-1]
         
-data = response.decode()  ## we have our response!
-place=data.find("<")
+    if(':' in input_address[7:]): #with a port number
+        portP=(input_address[7:]).find(':')
+        PORT=input_address[portP+8:-1]
+        PORT=int(PORT)
+        input_address=input_address[:portP+7]
 
-http_response_code = data[9:data.find("\n")] 
+    HOST = input_address[7:]  #sys.argv[1]- gets us the address  str()- makes it a string [7:]- gets rid of http://
 
-if ("200" in http_response_code):
-    print(data[place:])
+    
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    sock.connect_ex((HOST, PORT)) # connect to the host
+    website_name = "GET / HTTP/1.1\r\nHost:"+HOST+"\r\n\r\n"
 
-requests=0
+    sock.send(website_name.encode('utf-8')) #send the URL
 
-while True:
-    if requests==10:
+    response=sock.recv(4096) #get the response
+    while True:
+        data=sock.recv(4096)
+        if not data:
+            break
+        else:
+            response = response+data
+            
+    data = response.decode()  ## we have our response!
+
+    place=data.find("<") #beginning of the body part
+
+    http_response_code = data[9:data.find("\n")]
+    
+    print(http_response_code)
+
+    if ("200" in http_response_code):
+        print(data[place:])
+        system.exit(0)
+ 
+
+    if "301" in http_response_code:
+        loc_index = data.find("Location:")
+        temp_url = data[loc_index+10:]
+        end_index = temp_url.find("\n")
+        new_url = temp_url[:end_index]
+        sys.stderr.write("Redirected to:"+new_url)
+        requests=requests+1
+        print(new_url)
+        get_html(new_url,requests+1)
+            
+    if "302" in http_response_code:
+        requests=requests+1
+        loc_index = data.find("Location:")
+        temp_url = data[loc_index+10:]
+        end_index = temp_url.find("\n")
+        print(end_index)
+        new_url = temp_url[:end_index]
+        sys.stderr.write("Redirected to:"+new_url)
+        print("temp:"+temp_url+'\n')
+        print("new"+new_url+'\n')
+        get_html(new_url,requests+1)
+
+
+        
+    status=http_response_code[:3]
+
+    if int(status)>=400:
         sys.exit(1)
-    if ("301 Moved Permanently" in http_response_code):
-        loc_index = data.find("Location:")
-        new_url = data[loc_index+10:]
-        end_index = new_url.find("\n")
-        new_url = new_url[:end_index]
-        sys.stderr.write("Redirected to:"+new_url)
-        requests=requests+1
-        check_https(new_url)
-        
-    if("302" in http_response_code ):
-        requests=requests+1
-        loc_index = data.find("Location:")
-        new_url = data[loc_index+10:]
-        end_index = new_url.find("\n")
-        new_url = new_url[:end_index]
-        sys.stderr.write("Redirected to:"+new_url)
-        check_https(new_url)
-    else:
-        break
 
 
-    
-status=data[9:12]
-
-if int(status)>=400:
-    sys.exit(1)
-
-
-#if (start_index == -1):
-#     #error
-
-
-if ("200 OK" in data): 
-    sys.exit(0)
-else:
-    sys.exit(1)
-
+get_html(input_address,requests=0)
 #There should be a way to use status codes to help with the sorting of html stuff
 
 #Open TCP connection to a port
