64eb21af77ceae5f176997b7328d0ab3116231bb
AnnieXUZian <96567335+AnnieXUZian@users.noreply.github.com>
2023-01-20 00:12:45 -0600

    Update curl_clone.py

--- curl_clone.py
+++ curl_clone.py
@@ -4,10 +4,8 @@ import collections
 import os
 
 counter = 10
+PORT = 80
 
-def print_body(input_data):  #helper function to print only the body out
-  start_index = input_data.find("<body>")
-  end_index = input_data.find("</body>")
 
   output_message = "Header:"+HOST+"\n"+input_data[start_index:end_index+7]
   sys.stdout.write(output_message)
@@ -24,9 +22,15 @@ check_https(input_address)
 
 if (input_address[-1]=="/"):
     input_address = input_address[:-1]
+    
+if(':' in input_address[7:]): #with a port number
+    portP=(input_address[7:]).find(':')
+    PORT=input_address[portP+8:-1]
+    PORT=int(PORT)
+    input_address=input_address[:portP+7]
 
 HOST = input_address[7:]  #sys.argv[1]- gets us the address  str()- makes it a string [7:]- gets rid of http://
-PORT = 80
+
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -38,33 +42,52 @@ website_name = "GET / HTTP/1.1\r\nHost:"+HOST+"\r\n\r\n"
 
 sock.send(website_name.encode('utf-8'))
 
-response = sock.recv(4096)
+response=sock.recv(4096)
+while True:
+    data=sock.recv(4096)
+    if not data:
+        break
+    else:
+        response = response.append(data)
+        
 data = response.decode()  ## we have our response!
+place=data.find("<") 
 http_response_code = data[9:data.find("\n")] 
 
 if ("200" in http_response_code):
-    print_body(data)
+    print(data[place:])
 
-while ("301" in http_response_code and counter > 0):
-    counter = counter - 1
-    loc_index = data.find("Location:")
-    new_url = data[loc_index+10:]
-    end_index = new_url.find("\n")
-    new_url = new_url[:end_index]
-    sys.stderr.write("redirected to "+new_url)
-    check_https(new_url)
+requests=0
+while True:
+    if requests==10:
+        sys.exit(1)
+    if ("301 Moved Permanently" in http_response_code):
+        loc_index = data.find("Location:")
+        new_url = data[loc_index+10:]
+        end_index = new_url.find("\n")
+        response = new_url[:end_index]
+        sys.stderr.write("Redirected to:"+response)
+        requests=requests+1
+        check_https(new_url)
+    if("302" in http_response_code ):
+        requests=requests+1
+        loc_index = data.find("Location:")
+        new_url = data[loc_index+10:]
+        end_index = new_url.find("\n")
+        new_url = new_url[:end_index]
+        sys.stderr.write("Redirected to:"+response)
+        check_https(new_url)
+    else:
+        break
 
-while("302" in http_response_code and counter > 0):
-    counter = counter - 1
-    loc_index = data.find("Location:")
-    new_url = data[loc_index+10:]
-    end_index = new_url.find("\n")
-    new_url = new_url[:end_index]
-    print(new_url)
 
     
 
 
+status=data[9:12]
+
+if int(status)>=400:
+    sys.exit(1)
 
 
 #if (start_index == -1):
